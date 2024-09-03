9c40c9e2a29be7c8e99e3ae2ca466b9a9a291d2c
Sharma_aman_45 <amansharma6963@gmail.com>
2024-09-03 15:53:24 +0530

    new files

---
+++ includes/footer.php
@@ -0,0 +1,23 @@
+<div class="footer">
+    <div class="page-container footer-container">
+        <div class="footer-cities">
+            <div class="footer-city">
+                <a href="property_list.php?city=Delhi">hotel in Delhi</a>
+            </div>
+            <div class="footer-city">
+                <a href="property_list.php?city=Mumbai">hotel in Mumbai</a>
+            </div>
+            <div class="footer-city">
+                <a href="property_list.php?city=Bengaluru">hotel in Bangalore</a>
+            </div>
+            <div class="footer-city">
+                <a href="property_list.php?city=Hyderabad">hotel in Hyderabad</a>
+            </div>
+        </div>
+        <div class="footer-copyright">© Copyright vacations_xenonstack </div>
+    </div>
+</div>
+
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/common.js"></script>
